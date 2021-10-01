7d99e146c6afed08ddc0977b4f7ed82cd7c4087c
Raphaël Blanchet <rblanchet@mirego.com>
2021-10-01 10:56:49 -0400

    Fix on swift

--- swift-extensions/TrikotBluetoothScanResult.swift
+++ swift-extensions/TrikotBluetoothScanResult.swift
@@ -10,7 +10,7 @@ class TrikotBluetoothScanResult: NSObject, BluetoothScanResult {
 
     var bluetoothDevice: TrikotBluetoothDevice?
 
-    var name: String
+    var name: String?
 
     var physicalAddress: String
 
